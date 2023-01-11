0be28ebeceb44d6658db638ea06ab97918681b96
Ramil Samadov <90143263+Samedov2020@users.noreply.github.com>
2023-01-12 00:06:05 +0100

    Add files via upload

---
+++ for fun4/practise.js
@@ -0,0 +1,23 @@
+ try{
+  var div=document.querySelector("#container");
+  function renkayarla(){
+    var renksec=document.querySelector("#renksec").value;
+    if(renksec !="color"){
+      div.style.backgroundColor=renksec;
+    }
+  }
+  setInterval(() => {
+    let date= new Date()
+    let clock = document.getElementById('clock')
+    clock.innerHTML =
+    date.getHours()+" : "+
+    date.getMinutes ()+" : "+
+    date.getSeconds();
+    }, 1000);
+
+  
+  
+}
+catch(sikilmis){
+    document.write(sikilmis)
+}
